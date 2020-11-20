9cd2386e645185be662881ac9620d7dd8be20f93
taynahsilvazup <taynah.silva@zup.com.br>
2020-11-20 09:41:25 -0300

    commit inicial

---
+++ src/test/java/com/casaDoCodigo/casaDoCodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casaDoCodigo.casaDoCodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
